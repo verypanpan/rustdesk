6231d41fe2218ce3b94ca4bc00ed17d3efbc9c4b
verypanpan <63699905+verypanpan@users.noreply.github.com>
2024-10-31 00:31:10 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["erp.wecanintl.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "WDoDWpsgcvPbYFBijo7Q9txtupwxWj176+9Je0CoOSM=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.wecanintl.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "NjNNTpL1TgGpGo7O9ykahsh5j6JZG71mifOXG3e+ZZU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
